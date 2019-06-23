f21a7ee40c74c6f9e7cb2a8655bfc56266ada6ad
outsiderem <reemharbi.3@gmail.com>
2019-06-23 08:05:06 +0300

    done

--- mbta.js
+++ mbta.js
@@ -9,36 +9,76 @@ const stopsBetweenStations = function(startLine, startStation, endLine, endStati
         
         Orange: ['North Station', 'Haymarket', 'Park Street', 'State', 'Downtown Crossing', 'Chinatown', 'Back Bay', 'Forest Hills']
     }
+    let startStops = 0;
+    let endStops = 0;
 
-    let stops = 0;
-
-    for (var i=0; i<stations.lines.length; i++){
-
-
-
-      if (stations.lines.includes(startLine)){
-        if (stations.lines.includes(endLine))
-        {
-    console.log(`Rider boards the train at ${startLine} Line and ${startStation} Station.`); 
-    }   else 
-     { 
-                 return `Incorrect start line or end line. Please enter a valid line and try again.`;
-      }
-      }    
-    
-    if (startLine === 'Red'){
-      for (var i=0; i<stations.Red.length; i++){      
+      if (stations.lines.includes(startLine) && stations.lines.includes(endLine)){
+    if (startLine === 'Red'){          
       if (stations.Red.includes(startStation))
-        { stops++; } 
-        } 
-      }
+        {for (var i=0; i<stations.Red.indexOf(startStation); i++)
+        { console.log(`Rider boards the train at ${startLine} Line and ${startStation} Station.`);
+        startStops++;} } else 
+     {      console.log(`Incorrect start station or end station. Please enter a valid station and try again.`);
+     }
+    }
+      if (startLine === 'Green'){          
+      if (stations.Green.includes(startStation))
+        {for (var i=0; i<stations.Green.indexOf(startStation); i++){
+        console.log(`Rider boards the train at ${startLine} Line and ${startStation} Station.`);
+        startStops++;}          
+      } else 
+     { 
+                 console.log(`Incorrect start station or end station. Please enter a valid station and try again.`);
+     }
     }
 
+    if (startLine === 'Orange'){          
+      if (stations.Orange.includes(startStation))
+        {for (var i=0; i<stations.Orange.indexOf(startStation); i++){
+        console.log(`Rider boards the train at ${startLine} Line and ${startStation} Station.`);
+        startStops++;}          
+      } else 
+     { 
+                 console.log(`Incorrect start station or end station. Please enter a valid station and try again.`);
+     }
+    }  
     
-    console.log(`Number of stops: ${stops}`);
+    if (endLine === 'Red'){          
+      if (stations.Red.includes(endStation))
+        {for (var i=0; i<stations.Red.indexOf(endStation); i++){
+        endStops++;}          
+      } else 
+     { 
+                 console.log(`Incorrect start station or end station. Please enter a valid station and try again.`);
+     }
+    }
+      if (endLine === 'Green'){          
+      if (stations.Green.includes(endStation))
+        {for (var i=0; i<stations.Green.indexOf(endStation); i++){
+        endStops++;}          
+      } else 
+     { 
+                 console.log(`Incorrect start station or end station. Please enter a valid station and try again.`);
+     }
+    }
 
- }
+    if (endLine === 'Orange'){          
+      if (stations.Orange.includes(endStation))
+        {for (var i=0; i<stations.Orange.indexOf(endStation); i++){
+        endStops++;}          
+      } else 
+     { 
+                 console.log(`Incorrect start station or end station. Please enter a valid station and try again.`);
+     }
+    }
+      }  
+     else 
+     { 
+                 console.log(`Incorrect start line or end line. Please enter a valid line and try again.`);
+      }    
+    
 
-
-
-stopsBetweenStations('Red', 'Alewife', 'Orange', 'Alewife');
+    
+    console.log(`Number of stops: ${startStops + endStops}`);
+}
+stopsBetweenStations('Red', 'Park Street', 'Green', 'Park Street');
